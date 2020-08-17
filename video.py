02255e5055d853af29c648a0139c80ecc07acac1
shahbaz-pycread <alam.shahbaz01@gmail.com>
2020-08-17 13:15:14 +0530

    first commit

---
+++ video.py
@@ -0,0 +1,21 @@
+#importing module
+import pytube
+
+
+
+
+#url of the video to be downloaded
+video_url = input('Please enter the link of the video: ')
+
+#load url in function Youtube
+
+yt = pytube.YouTube(video_url)
+
+# Will display the first the resolution of the video.
+
+video = yt.streams.first()
+
+
+# Download video
+video.download('C:/vidoes')
+print('Download Completed')
